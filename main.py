34de1051679c0540db127f8e7eda2cc2aea8ecd9
SandakelumElvitigala <kalpaselvitigala@gmail.com>
2025-04-17 04:42:37 +0530

    Update main

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ async def ask_question(question: str = Form(...)):
 
 # Main function to run the app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
